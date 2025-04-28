bf5cf117e2e225de9f125808ef8c81dc679558e7
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2025-04-28 11:45:12 +0200

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+const recipeArray = [
+  {
+    src: './assets/images/instagram_01.jpg',
+    alt: 'Blabla',
+    recipe: '1BLABLALBLABLALBALBLABLA JAJAJAJAJAJAJAJ',
+  },
+  {
+    src: './assets/images/instagram_02.jpg',
+    alt: 'Blabla',
+    recipe: '2BLABLALBLABLALBALBLABLA JAJAJAJAJAJAJAJ',
+  },
+  {
+    src: './assets/images/instagram_03.jpg',
+    alt: 'Blabla',
+    recipe: '3BLABLALBLABLALBALBLABLA JAJAJAJAJAJAJAJ',
+  },
+  {
+    src: './assets/images/instagram3.jpg',
+    alt: 'Blabla',
+    recipe: '4BLABLALBLABLALBALBLABLA JAJAJAJAJAJAJAJ',
+  },
+  {
+    src: './assets/images/instagram5.jpg',
+    alt: 'Blabla',
+    recipe: '5BLABLALBLABLALBALBLABLA JAJAJAJAJAJAJAJ',
+  },
+  {
+    src: './assets/images/instagram7.jpg',
+    alt: 'Blabla',
+    recipe: '6BLABLALBLABLALBALBLABLA JAJAJAJAJAJAJAJ',
+  },
+];
